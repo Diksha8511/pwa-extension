b57e3c453b25d62ae076d71a5f6a77d0d6a2ebee
Diksha Pandit <diksha.pandit@krishtechnolabs.com>
2023-08-14 15:36:15 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/**
+ * Custom index for the extension attention this file should be not delete!
+ * It is use as main file but this can be empty by default only need to exits.
+ *
+ * A project index.js should contain default exports like:
+ * export { default } from './components/main';
+ */
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            Blog extension
+        </div>
+    );
+}
+
+export default Index;
